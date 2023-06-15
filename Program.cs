977e612f977d5a7ce0622dbc9ac73e8019e827aa
guilherme <longo.guilherme@hotmail.com>
2023-06-14 22:41:40 -0300

    .Net 6 Web API

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/user", () => new {Name = "Antonio", Age = 35} );
+
+app.MapPost("/Saveprod", (Prod product) => {
+    return product.Name + " - " + product.Cod;
+});
+
+app.MapGet("/AddHeader", (HttpResponse response) =>{
+response.Headers.Add("Teste", "Testando header");
+return "Todo mundo é gay";
+});
+
+//FromQuery url
+app.MapGet("/getproduct",([FromQuery] string dateStart, [FromQuery] string dateEnd) =>{
+  return dateStart + " - " + dateEnd;
+});
+
+//user/{code}
+app.MapGet("/getproduct/{Code}", ([FromRoute]string Code) =>{
+return Code;
+});
+
+//Usando Request Header key/value //tem q ser o mesmo nome daqui na key "product-code"
+app.MapGet("/getprod", (HttpRequest request) =>{
+    return request.Headers["product-code"].ToString();
+});
+
+app.Run();
+
+public class Prod
+{
+public string Name { get; set; }
+public string Cod { get; set; }
+}
